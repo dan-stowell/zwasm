23c56d9d030026308c4f7bfa6ff503ba70aff7e0
Dan Stowell <dstowell@gmail.com>
2023-06-26 22:38:01 -0500

    (not yet working) check magic number, binary version

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,24 @@ pub fn main() !void {
         std.debug.print("Could not open module path {s}: {!}", .{ module_path, err });
         return;
     };
+    defer wasmfile.close();
+
     const stats = try wasmfile.stat();
     std.debug.print("The module file is {d} bytes\n", .{stats.size});
+
+    var buffer: [4]u8 = undefined;
+    const bytes_read = wasmfile.read(&buffer) catch |err| {
+        std.debug.print("Could not read file: {!}", .{err});
+        return;
+    };
+    std.debug.print("Read {d} bytes\n", .{bytes_read});
+    if ((0x0061 == buffer[0]) and (0x736d == buffer[1])) {
+        std.debug.print("Correct magic number!\n", .{});
+    } else {
+        std.debug.print("Incorrect magic number!\n", .{});
+    }
+    const version = buffer[2];
+    std.debug.print("Binary version {x}\n", .{version});
 }
 
 test "simple test" {
